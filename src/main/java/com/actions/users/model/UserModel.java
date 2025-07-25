30f720c8cc614a439499fe554665bc77f0d846dc
Angel Colin <angel.antoni48@gmail.com>
2025-07-25 10:58:07 -0600

    feat(entity) agrego entidad User

---
+++ src/main/java/com/actions/users/model/UserModel.java
@@ -0,0 +1,21 @@
+package com.actions.users.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String lastname;
+    private String email;
+}
