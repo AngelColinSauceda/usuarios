45d870dadb1516d5b971ae6f599512078a999dd5
Angel Colin <angel.antoni48@gmail.com>
2025-07-25 11:03:28 -0600

    feat(repository): agrego UserRepository

---
+++ src/main/java/com/actions/users/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.actions.users.repository;
+
+import com.actions.users.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel,String> {
+
+}
