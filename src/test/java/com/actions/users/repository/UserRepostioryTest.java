0d0f8d8a4c83efbd1345c28a451afb912a3ae4c1
Angel Colin <angel.antoni48@gmail.com>
2025-08-02 12:48:07 -0600

    test(repository): agrega prueba findById en UserRepository

---
+++ src/test/java/com/actions/users/repository/UserRepostioryTest.java
@@ -0,0 +1,30 @@
+package com.actions.users.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.actions.users.model.UserModel;
+
+@DataJpaTest
+public class UserRepostioryTest {
+    
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void findByIdTest(){
+     UserModel user = new UserModel(null, "Angel", "Colin", "angel.antoni48@gmail.com");
+     UserModel userSave = repository.save(user);
+
+     String idGenerado = userSave.getId();
+
+     var userFindById = repository.findById(idGenerado);
+     
+     assertTrue(userFindById.isPresent());
+     assertEquals("Angel", userFindById.get().getName());
+    }
+}
