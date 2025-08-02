73822dfb7d1e1a3212094de986d716701d6113cb
Angel Colin <angel.antoni48@gmail.com>
2025-08-02 12:53:06 -0600

    fead(repository) agrega findByEmail

--- src/main/java/com/actions/users/repository/UserRepository.java
+++ src/main/java/com/actions/users/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.actions.users.repository;
 
 import com.actions.users.model.UserModel;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel,String> {
-
+ Optional<UserModel> findByEmail(String email);
 }
